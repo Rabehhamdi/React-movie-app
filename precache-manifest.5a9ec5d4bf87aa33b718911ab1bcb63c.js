4e26f077dfbbf16a77d596c2cbdb8c55e9c133e3
HamdiRabeh <46111136+Rabehhamdi@users.noreply.github.com>
2019-07-10 14:51:19 +0100

    Updates

---
+++ precache-manifest.5a9ec5d4bf87aa33b718911ab1bcb63c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89c51862fb94abb0e1955b36c6ebf35e",
+    "url": "/.github.io/React-movie-app/index.html"
+  },
+  {
+    "revision": "4b83b3cd3b01ccdee7ba",
+    "url": "/.github.io/React-movie-app/static/css/main.d4d973e5.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/.github.io/React-movie-app/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "4b83b3cd3b01ccdee7ba",
+    "url": "/.github.io/React-movie-app/static/js/main.e8749ddc.chunk.js"
+  },
+  {
+    "revision": "35628908fee15c318a3f",
+    "url": "/.github.io/React-movie-app/static/js/runtime~main.bc5d2c8d.js"
+  }
+]);
